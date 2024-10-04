49a9bb5a11e97f1a6e7081301d8a18102ea3425d
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-10-04 21:24:11 +0200

    feat: write a function delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,33 @@
+import { access, constants, unlink } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filesDir = join(__dirname, 'files');
+const fileToRemove = join(filesDir, 'fileToRemove.txt');
+
+//проверяем существование файла.
+const isExistsFile = async(fileName) => {
+    try{
+        await access(fileName, constants.F_OK);
+        return true;
+    } catch (error) {
+        console.error('FS operation failed: fileToRemove.txt does not exist');
+        return false;
+    }
+}
+
 const remove = async () => {
-    // Write your code here 
+    const exists = await isExistsFile(fileToRemove);
+    if (!exists) return;
+
+    try {
+        await unlink(fileToRemove);
+        console.log('File deleted successfully!');
+    } catch (error) {
+        console.error('FS operation failed: ', error.message);
+    }
 };
 
 await remove();
