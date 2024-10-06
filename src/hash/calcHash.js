c88e81fdbb47246f0c91ad3e9e377642e1e7160b
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-10-06 18:08:26 +0200

    feat: write a function calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,43 @@
+import { access, constants } from 'fs/promises';
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToHash = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+//проверяем существование файла.
+const isExistsFile = async(fileName) => {
+    try{
+        await access(fileName, constants.F_OK);
+        return true;
+    } catch (error) {
+        console.error('Operation failed: file does not exist');
+        return false;
+    }
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const existFile = await isExistsFile(fileToHash);
+        if (!existFile) return;
+
+        // Создаем хэш для SHA256
+        const hash = createHash('sha256');
+
+        const stream = createReadStream(fileToHash);
+        stream.on('data', (chunk) => hash.update(chunk));
+        stream.on('end', () => {
+            const res = hash.digest('hex');
+            console.warn(`SHA256 hash (hex): ${res}`)
+        });
+        stream.on('error', (error) => console.error(`Error: ${error}`));
+        
+    } catch (error) {
+        console.error('Operation failed: ', error.massage);
+    }
 };
 
 await calculateHash();
