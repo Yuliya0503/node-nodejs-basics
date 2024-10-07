19a8fad2257d6c08d50cdde71ae6870fa316c18d
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-10-07 13:52:59 +0200

    feat: add solution task child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import { spawn } from 'child_process';
+import { stdin, stdout } from 'process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = join(__dirname, 'files', 'script.js');
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    // Создаем дочерний процесс
+    const child = spawn('node', [file, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe'] // Устанавливаем stdio для IPC
+    });
+    //передаем данные из родительского stdin в дочерний
+    stdin.pipe(child.stdin);
+
+    //передаем из дочернего stdout в родительский
+    child.stdout.pipe(stdout);
+
+    child.stderr.on('data', (data) => {
+        console.error('Error from child process: ', data.toString());
+    });
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    })
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2', 'someArgument2', 'someArgument2', 'someArgument25555', 'dffdfd'] );
