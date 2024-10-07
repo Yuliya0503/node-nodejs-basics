4634f94b8373ede4f9932a114c7258a55ec831ec
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-10-07 11:58:56 +0200

    feat: add functions stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,36 @@
+import { access, constants, open } from 'fs/promises';
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToWrite = join(__dirname, 'files', 'fileToWrite.txt');
+
+//проверяем существование файла.
+const isExistsFile = async(fileName) => {
+    try {
+        await access(fileName, constants.F_OK);
+        console.log('File exists, writing will overwrite it.');
+    } catch (error) {
+        console.log('File does not exist, creating a new one...');
+        await open(fileName, 'w'); // создаем файл, если его нет
+    }
+}
+
 const write = async () => {
-    // Write your code here 
+    try {
+        await isExistsFile(fileToWrite); 
+
+        const stream = createWriteStream(fileToWrite);
+        process.stdin.pipe(stream);
+        process.stdin.on('end', () => {
+            console.warn('\nFinished writing data to file.');
+        });
+        stream.on('error', error => console.error('Operation failed: ', error.message));
+    } catch (error) {
+        console.error('Compression failed:', error.message);
+    }
 };
 
 await write();
